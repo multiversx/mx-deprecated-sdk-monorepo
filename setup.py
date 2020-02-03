aed6f034a4826798d983c94522eb0573a388dacb
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-03 10:48:13 +0200

    Setup.py configuration.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+import setuptools
 
-setup(name="Elrond-SmartContracts",
-      version="0.1",
+setuptools.setup(name="Elrond-SmartContracts",
+      version="0.0.1",
       description="Elrond Smart Contracts Tools",
       url="https://github.com/ElrondNetwork/elrond-sc-tools",
       author="Elrond Network",
       license="GPL",
       packages=setuptools.find_packages(),
-      zip_safe=False)
+      zip_safe=False,
+      python_requires=">=3.6")
