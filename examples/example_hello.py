50f0d1ee00eead0f550a7ef020ea5976d9b581b3
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-07 15:52:57 +0200

    Fix example.

--- examples/example_hello.py
+++ examples/example_hello.py
@@ -24,25 +24,25 @@ if __name__ == '__main__':
     bytecode = codebase.get_bytecode()
     logger.info("Bytecode: %s", bytecode)
 
-    # Now, we can deploy the smart contract on node-debug.
-    contract = SmartContract(bytecode=bytecode)
+    # Now, we create a gateway which intermediates deployment and execution
     gateway = DebugGateway()
+    # We initialize the smart contract with the compiled bytecode.
+    contract = SmartContract(bytecode=bytecode)
 
-    answer = None
-
+    # A flow defines the desired steps to interact with the contract.
     def myflow():
+        # First, we deploy the contract in the name of Alice.
         alice = "aaaaaaaa112233441122334411223344112233441122334411223344aaaaaaaa"
         tx, address = gateway.deploy_contract(contract, sender=alice)
-        
-        assert address is not None
         logger.info("Tx hash: %s", tx)
         logger.info("Contract address: %s", address)
         
+        # Secondly, we execute a pure function of the contract.
         answer = gateway.query_contract(contract, "getUltimateAnswer")[0]
-        answer_bytes = base64.decodebytes(b"Kg==")
+        answer_bytes = base64.b64decode(answer)
         answer_hex = answer_bytes.hex()
         answer_int = int(answer_hex, 16)
-        logger.info(f"Answer: {answer}, {answer_bytes}, {answer_hex}, {answer_int}")
+        logger.info(f"Answer: {answer_bytes}, {answer_hex}, {answer_int}")
 
 
     gateway.run_flow(myflow)
