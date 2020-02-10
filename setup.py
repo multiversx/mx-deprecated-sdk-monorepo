958c4104452146c35ff009e86b2ce8bd3e2a5cc2
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-10 19:33:06 +0200

    Update setup.

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
 import setuptools
 
-setuptools.setup(name="erdpy",
-      version="0.0.1",
-      description="Elrond Smart Contracts Tools",
-      url="https://github.com/ElrondNetwork/elrond-sc-tools",
-      author="Elrond Network",
-      license="GPL",
-      packages=setuptools.find_packages(),
-      install_requires=[
-          "toml",
-      ],
-      zip_safe=False,
-      python_requires=">=3.6")
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="erdpy",
+    version="0.0.1",
+    description="Elrond Smart Contracts Tools and Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ElrondNetwork/erdpy",
+    author="Elrond Network",
+    license="GPL",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "toml",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha"
+    ],
+    python_requires=">=3.6"
+)
