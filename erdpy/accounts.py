c7625604b9146d4e9d53cb7186f8b1c3958241ad
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-12 15:48:15 +0200

    Bugfix read pem file.

--- erdpy/accounts.py
+++ erdpy/accounts.py
@@ -4,7 +4,9 @@ from erdpy import utils
 
 class Account:
     def __init__(self, address, pem_file=None):
-        pem_file = path.expanduser(pem_file)
-
         self.address = address
-        self.pem = utils.read_file(pem_file) if pem_file is not None else None
+        self.pem = None
+
+        if pem_file:
+            pem_file = path.expanduser(pem_file)
+            self.pem = utils.read_file(pem_file)
