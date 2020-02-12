3ec81fd32029fc6f5d72a0e06e7a5dcbe0fff177
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-12 15:16:32 +0200

    Bigfix: expanduser path when reading PEM files.

--- erdpy/accounts.py
+++ erdpy/accounts.py
@@ -1,7 +1,10 @@
+from os import path
 from erdpy import utils
 
 
 class Account:
     def __init__(self, address, pem_file=None):
+        pem_file = path.expanduser(pem_file)
+
         self.address = address
         self.pem = utils.read_file(pem_file) if pem_file is not None else None
