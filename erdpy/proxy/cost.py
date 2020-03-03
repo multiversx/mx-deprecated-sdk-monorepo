e1b296f209a43a6f6d1f61fc3fd95b54b772310e
Iuga Mihai <mihai9625@gmail.com>
2020-03-03 09:29:03 +0200

    EN-5821 : fix after review and update version

--- erdpy/proxy/cost.py
+++ erdpy/proxy/cost.py
@@ -27,6 +27,8 @@ def prepare_argument(argument):
 
 
 class TransactionCostEstimator:
+    # needs these constant because when the observer node receive a post request from proxy with a transaction needs
+    # to can figure out what type of transaction was send to can calculate an estimation
     _SENDER_ADDRESS = "e3784da068cca901c0c629b304d024eb777fdf604dd8f6b5c0dc0c7f75877473"
     _RECEIVER_ADDRESS = "e3784da068cca901c0c629b304d024eb777fdf604dd8f6b5c0dc0c7f75877471"
 
