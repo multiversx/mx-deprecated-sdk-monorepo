82d1b0f6d0496f300717a3e05fa9b0f7bb828e9b
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-05 22:26:20 +0300

    erdjs - initial commit.

---
+++ erdjs/index.js
@@ -0,0 +1,3 @@
+exports.deployContract = function () {
+    console.warn("todo");
+}
--- examples/mocha-testing/test/testHello.js
+++ examples/mocha-testing/test/testHello.js
@@ -1,9 +1,16 @@
-var assert = require('assert');
+var assert = require("assert");
+var erdjs = require("erdjs");
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
+describe("Array", function () {
+    describe("#indexOf()", function () {
+        it("should return -1 when the value is not present", function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+
+    describe("#foobar()", function () {
+        it("should deploy and run smart contract", function () {
+            erdjs.deployContract();
         });
     });
 });
