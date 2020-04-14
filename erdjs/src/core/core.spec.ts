6992eeccc3a70a1319faf6ce05faa8ab38b16185
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-04-14 16:22:21 +0300

    Create proper fetchWithTimeout()

--- erdjs/src/core/core.spec.ts
+++ erdjs/src/core/core.spec.ts
@@ -1,57 +1,16 @@
 import { equal } from "assert";
-import { MyModel } from "./core";
-
-import fetch, {
-    Blob,
-    Headers,
-    Request,
-    RequestInit,
-    RequestInfo,
-    Response,
-    FetchError
-} from "node-fetch";
-
-describe("Typescript usage suite", () => {
-    it("should be able to execute a test", () => {
-        let model = new MyModel()
-        equal(42, model.Get());
-    });
-});
+import { fetchWithTimeout } from "./core";
+import { Response } from "node-fetch";
 
 describe("Proxy try-out", () => {
     it("should get the balance of an address", () => {
-        let address: string = "14dcb8b9aaa0069fc600f08938a130ebba4a9b691dfbd6775bd59cbd052c94ca";
+        let address: string = "18df0234a57bf1ce145eb7f4924b065643de7e9adb5895651abf0b83295930f1";
         console.log(address);
 
-        let url: string = `http://zirconium:7950/address/${address}/balance`;
+        let url: string = `http://zirconium:7950/address/${address}`;
         fetchWithTimeout(url, 1000)
             .then((response: Response) => response.json())
             .then((json: string) => console.log(json))
             .catch((err: Error) => console.log(err));
     });
 });
-
-function stringToHex(str: string): string {
-    const chars = [...str];
-    return chars.map(chr => chr.charCodeAt(0).toString(16).padStart(2, "0")).join("");
-}
-
-function bytesToHex(bytes: Array<number>): string {
-    return bytes.map(byte => byte.toString(16).padStart(2, "0")).join("");
-}
-
-function fetchWithTimeout(resource: RequestInfo, timeout: number, init?: RequestInit): Promise<Response> {
-    return new Promise<Response>((resolve, reject) => {
-        const timeoutID = setTimeout(
-            () => reject(new Error('Request timed out')),
-            timeout
-        );
-
-        fetch(resource)
-            .then(
-                (response: Response) =>  resolve(response),
-                (err: Error) => reject(err)
-            )
-            .finally(() => clearTimeout(timeoutID));
-    });
-}
--- erdjs/src/core/core.ts
+++ erdjs/src/core/core.ts
@@ -1,7 +1,61 @@
-import { Model } from "backbone"
+import fetch, {
+    Blob,
+    Headers,
+    Request,
+    RequestInit,
+    RequestInfo,
+    Response,
+    FetchError
+} from "node-fetch";
 
-export class MyModel extends Model {
-    public Get() {
-        return 42;
+import { AbortController, AbortSignal } from "abort-controller";
+
+export function fetchWithTimeout(resource: RequestInfo, timeout: number, init?: RequestInit): Promise<Response> {
+    const timeoutController = new TimeoutController(timeout);
+
+    if (init == null) {
+        init = {}; 
     }
+    if (init.signal == null) {
+        init.signal = timeoutController.start();
+    }
+
+    let fetchPromise = fetch(resource, init);
+    fetchPromise.finally(() => timeoutController.clear());
+    return fetchPromise;
+}
+
+export class TimeoutController extends AbortController {
+    private timer: NodeJS.Timer;
+    private timeout: number;
+
+    constructor(t: number) {
+        super();
+        this.timeout = t;
+    }
+
+    start(): AbortSignal {
+        if (this.signal.aborted) {
+            throw new Error(`TimeoutController already aborted`);
+        }
+        this.timer = setTimeout(
+            () => this.abort(),
+            this.timeout
+        );
+
+        return this.signal;
+    }
+
+    clear() {
+        clearTimeout(this.timer);
+    }
+}
+
+export function stringToHex(str: string): string {
+    const chars = [...str];
+    return chars.map(chr => chr.charCodeAt(0).toString(16).padStart(2, "0")).join("");
+}
+
+export function bytesToHex(bytes: Array<number>): string {
+    return bytes.map(byte => byte.toString(16).padStart(2, "0")).join("");
 }
