1a8e8e265e671f5958ac22b85f78db7c726dd684
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-24 10:37:14 +0300

    Remove "only" flag.

--- erdjs/src/testing/world.spec.ts
+++ erdjs/src/testing/world.spec.ts
@@ -2,7 +2,7 @@ import { describe } from "mocha"
 import { World } from "./world"
 import assert = require("assert");
 
-describe.only("test world", () => {
+describe("test world", () => {
     it("should create account", async () => {
         let world = new World("foo");
         let result = await world.createAccount("alice", "100", 42);
