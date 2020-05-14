e00159ed519fcbdbd1b4bc520158e522369dfc58
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-05-14 11:23:06 +0300

    Adjust default gas price.

--- erdpy/config.py
+++ erdpy/config.py
@@ -3,5 +3,5 @@ DOWNLOAD_MIRROR = "https://ide.elrond.com"
 
 # Configuration that may be edited by the end user:
 ROOT_FOLDER_NAME = "ElrondSCTools"
-DEFAULT_GASPRICE = 200000000000000
+DEFAULT_GASPRICE = 200000000000
 DEFAULT_GASLIMIT = 50000000
