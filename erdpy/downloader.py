a4419780a0cca192cca62d764d5f71296108c2dc
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-07-04 22:12:08 +0300

    Friendlier download progress.

--- erdpy/downloader.py
+++ erdpy/downloader.py
@@ -22,11 +22,13 @@ def download(url, filename):
         response.raise_for_status()
         total_size = int(response.headers.get("content-length", 0))
         chunk_number = 0
+        progress = 0
         with open(filename, 'wb') as file:
             for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
                 file.write(chunk)
-                _report_download_progress(chunk_number, total_size)
+                progress = _report_download_progress(progress, chunk_number, total_size)
                 chunk_number += 1
+            print()
     except requests.HTTPError as err:
         raise errors.DownloadError(
             f"Could not download [{url}] to [{filename}]") from err
@@ -34,7 +36,9 @@ def download(url, filename):
     logger.info("Download done.")
 
 
-def _report_download_progress(chunk_number, total_size):
+def _report_download_progress(progress, chunk_number, total_size):
     num_chunks = total_size / CHUNK_SIZE + 1
-    progress = int(chunk_number / num_chunks * 100)
-    print(f"{progress} %", end="\r", file=sys.stderr)
+    new_progress = int(chunk_number / num_chunks * 100)
+    if new_progress != progress:
+        print("#", end='', flush=True, file=sys.stderr)
+    return new_progress
