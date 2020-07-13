3b5207693101822142f4bc968ac441dde808bba7
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-07-13 23:58:52 +0300

    Fix CLI, add tests.

--- erdwalletjs-cli/index.js
+++ erdwalletjs-cli/index.js
@@ -23,14 +23,19 @@ function setupCli(program) {
     program.name("erdwalletjs")
 
     program
-        .command("new")
-        .description("Create a wallet based on a new or existing mnemonic phrase")
-        .option("--in-mnemonic-file <inMnemonicFile>", "the file containing an existing mnemonic")
-        .option("--out-mnemonic-file <outMnemonicFile>", "where to save the new mnemonic, if it's the case")
-        .option("--account-index <accountIndex>", "the index of the wallet to derive", "0")
-        .option("--password-file <passwordFile>", "the file containing the key-file password")
-        .option("--key-file <keyFile>", "where to save the key-file")
-        .action(tryAction(newWallet));
+        .command("new-mnemonic")
+        .description("Create a new mnemonic phrase (24 words)")
+        .requiredOption("-m, --mnemonic-file <mnemonicFile>", "where to save the mnemonic")
+        .action(newMnemonic);
+
+    program
+        .command("derive-key")
+        .description("Derive a JSON key-file from an existing mnemonic phrase")
+        .requiredOption("-m, --mnemonic-file <mnemonicFile>", "a file containing the mnemonic")
+        .option("-n, --account-index <accountIndex>", "the account index to derive", "0")
+        .requiredOption("-k, --key-file <keyFile>", "the key-file to create")
+        .requiredOption("-p, --password-file <passwordFile>", "a file containing the password for the key-file")
+        .action(deriveKey);
 
     program
         .command("sign")
@@ -39,71 +44,37 @@ function setupCli(program) {
         .requiredOption("-o, --out-file <outFile>", "where to save the signed JSON transaction")
         .requiredOption("-k, --key-file <keyFile>", "the key-file (the wallet)")
         .requiredOption("-p, --password-file <passwordFile>", "the file containing the key-file password")
-        .action(tryAction(signMessage));
+        .action(signMessage);
 }
 
-function tryAction(action) {
-    return function (cmdObj) {
-        try {
-            action(cmdObj);
-        } catch (error) {
-            if (error instanceof UsageError) {
-                console.error(`Usage error: ${error.message}`);
-                cmdObj.help();
-            } else {
-                throw error;
-            }
-        }
-    }
-}
-
-function newWallet(cmdObj) {
-    let passwordFile = asUserPath(cmdObj.passwordFile);
-    let keyFile = asUserPath(cmdObj.keyFile);
-    let accountIndex = parseInt(cmdObj.index) || 0;
-
-    let mnemonic = getOrGenerateMnemonic(cmdObj);
-
-    if (!keyFile) {
-        // Mnemonic generated and displayed, but no keyfile will be generated.
-        return;
-    }
-
-    let password = readText(passwordFile);
-    let keyFileJson = deriveWallet(mnemonic, accountIndex, password);
-
-    writeToNewFile(keyFile, `${keyFileJson}\n`)
-}
-
-function getOrGenerateMnemonic(cmdObj) {
-    let inMnemonicFile = asUserPath(cmdObj.inMnemonicFile);
-    let outMnemonicFile = asUserPath(cmdObj.outMnemonicFile);
-
-    if (inMnemonicFile) {
-        return readText(inMnemonicFile);
-    }
-
-    if (!outMnemonicFile) {
-        throw new UsageError("Should specify where to save the mnemonic.");
-    }
-
+function newMnemonic(cmdObj) {
+    let mnemonicFile = asUserPath(cmdObj.mnemonicFile);
     let account = new core.account();
     let mnemonic = account.generateMnemonic();
 
-    writeToNewFile(outMnemonicFile, `${mnemonic}\n`)
-    console.log(`Mnemonic saved to file: ${outMnemonicFile}.`);
+    writeToNewFile(mnemonicFile, `${mnemonic}\n`)
+    console.log(`Mnemonic saved to file: [${mnemonicFile}].`);
     console.log(`\n${mnemonic}\n`);
-
-    return mnemonic;
 }
 
-function deriveWallet(mnemonic, accountIndex, password) {
+function deriveKey(cmdObj) {
+    let mnemonicFile = asUserPath(cmdObj.mnemonicFile);
+    let accountIndex = parseInt(cmdObj.accountIndex) || 0;
+    let keyFile = asUserPath(cmdObj.keyFile);
+    let passwordFile = asUserPath(cmdObj.passwordFile);
+    
+    let mnemonic = readText(mnemonicFile);
+    let password = readText(passwordFile);
+    
     let account = new core.account();
     let privateKeyHex = account.privateKeyFromMnemonic(mnemonic, false, accountIndex.toString(), "");
     let privateKey = Buffer.from(privateKeyHex, "hex");
     let keyFileObject = account.generateKeyFileFromPrivateKey(privateKey, password);
     let keyFileJson = JSON.stringify(keyFileObject, null, 4);
-    return keyFileJson;
+
+    console.log(`Derived key for account index = ${accountIndex}, address = ${account.address()}.`);
+    console.log(`Encrypted with password from [${passwordFile}] and saved to file: [${keyFile}].`);
+    writeToNewFile(keyFile, `${keyFileJson}\n`);
 }
 
 function signMessage(cmdObj) {
@@ -120,10 +91,10 @@ function signMessage(cmdObj) {
 
     let account = new core.account();
     account.loadFromKeyFile(keyFileObject, password);
-
+    
     let transaction = new core.transaction(
         message.nonce,
-        message.sender,
+        account.address(),
         message.receiver,
         message.value,
         message.gasPrice,
@@ -155,12 +126,4 @@ function writeToNewFile(filePath, content) {
     }
 
     fs.writeFileSync(filePath, content);
-}
-
-function UsageError(message) {
-    this.message = message;
-
-    this.toString = function () {
-        return message;
-    }
 }
