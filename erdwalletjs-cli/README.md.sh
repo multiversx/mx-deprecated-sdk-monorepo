e7848cc6452abe794ef1d8e32b79907837dbad37
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-07-14 00:05:22 +0300

    Rename CLI.md to README.md.

---
+++ erdwalletjs-cli/README.md.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+WALLETJS="./index.js"
+
+code() {
+    printf "\n\`\`\`\n" >> README.md
+}
+
+cliCommand() {
+    printf "### $1\n\n" >> README.md
+
+    code
+    printf "$ erdwalletjs $2 --help\n" >> README.md
+    ${WALLETJS} ${2} --help >> README.md
+    code
+    printf "\n\n" >> README.md
+}
+
+generate() {
+    echo -n > README.md
+    printf "# Command Line Interface\n\n" >> README.md
+
+    printf "## Overview\n\n" >> README.md
+    printf "**erdwalletjs** exposes the following **commands**:\n\n" >> README.md
+
+    code
+    printf "$ erdwalletjs --help\n" >> README.md
+    ${WALLETJS} --help >> README.md
+    code
+
+    cliCommand "New Mnemonic" "new-mnemonic"
+    cliCommand "Derive Key File" "derive-key"
+    cliCommand "Sign" "sign"
+}
+
