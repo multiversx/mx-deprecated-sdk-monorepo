d8295e83e94951973d62bfa42734e7eae395928b
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-08-04 18:10:41 +0300

    Sketch web tools.

---
+++ docs/src/app.ts
@@ -0,0 +1,12 @@
+
+import { Address } from "@elrondnetwork/erdjs";
+
+declare var $: any;
+
+$(document).ready(async function () {
+    main();
+});
+
+function main() {
+    console.log("Tools");
+}
