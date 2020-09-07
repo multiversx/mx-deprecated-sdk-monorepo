23d0b5793026fccfed0218170effe8b9af36594f
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-09-08 00:50:47 +0300

    Wallet (java) - sketch implementation.

---
+++ erdjava/src/main/java/elrond/Wallet.java
@@ -0,0 +1,94 @@
+package elrond;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.bouncycastle.crypto.digests.SHA512Digest;
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+
+public class Wallet {
+    static final String BIP39_SALT_MODIFIER = "mnemonic";
+    static final int BIP39_PBKDF2_ROUNDS = 2048;
+    static final String BIP32_SEED_MODIFIER = "ed25519 seed";
+    static final long[] ELROND_DERIVATION_PATH = { 44, 508, 0, 0, 0 };
+    static final long HARDENED_OFFSET = 0x80000000;
+
+    private Wallet() {
+    }
+
+    public static byte[] mnemonicToBip39Seed(final String mnemonic) {
+        final var mnemonicBytes = mnemonic.getBytes(StandardCharsets.UTF_8);
+        final var passphrase = BIP39_SALT_MODIFIER.getBytes(StandardCharsets.UTF_8);
+        final var generator = new PKCS5S2ParametersGenerator(new SHA512Digest());
+
+        generator.init(mnemonicBytes, passphrase, BIP39_PBKDF2_ROUNDS);
+        final byte[] seed = ((KeyParameter) generator.generateDerivedParameters(512)).getKey();
+        return seed;
+    }
+
+    public static byte[] bip39SeedToPrivateKey(byte[] seed, long accountIndex) throws IOException {
+        var keyAndChainCode = bip39SeedToMasterKey(seed);
+        var key = keyAndChainCode.key;
+        var chainCode = keyAndChainCode.chainCode;
+
+        long[] derivationPath = Arrays.copyOf(ELROND_DERIVATION_PATH, ELROND_DERIVATION_PATH.length);
+        derivationPath[derivationPath.length - 1] = accountIndex;
+
+        for (long segment : derivationPath) {
+            keyAndChainCode = ckdPriv(key, chainCode, segment + HARDENED_OFFSET);
+            key = keyAndChainCode.key;
+            chainCode = keyAndChainCode.chainCode;
+        }
+
+        return key;
+    }
+
+    public static KeyAndChainCode bip39SeedToMasterKey(byte[] seed) {
+        byte[] result = hmacSHA512(BIP32_SEED_MODIFIER.getBytes(StandardCharsets.UTF_8), seed);
+        byte[] masterKey = Arrays.copyOfRange(result, 0, 32);
+        byte[] chainCode = Arrays.copyOfRange(result, 32, 64);
+        return new KeyAndChainCode(masterKey, chainCode);
+    }
+
+    static class KeyAndChainCode {
+        public final byte[] key;
+        public final byte[] chainCode;
+
+        public KeyAndChainCode(byte[] key, byte[] chainCode) {
+            this.key = key;
+            this.chainCode = chainCode;
+        }
+    }
+
+    private static KeyAndChainCode ckdPriv(byte[] key, byte[] chainCode, long index) throws IOException {
+        ByteBuffer indexBuffer = ByteBuffer.allocate(4);
+        indexBuffer.order(ByteOrder.BIG_ENDIAN);
+        indexBuffer.putInt((int) (index & 0xffffffffL));
+        byte[] indexBytes = indexBuffer.array();
+
+        ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+        dataStream.write(new byte[] { 0 });
+        dataStream.write(key);
+        dataStream.write(indexBytes);
+        byte[] data = dataStream.toByteArray();
+        byte[] result = hmacSHA512(chainCode, data);
+
+        return new KeyAndChainCode(Arrays.copyOfRange(result, 0, 32), Arrays.copyOfRange(result, 32, 64));
+    }
+
+    public static byte[] hmacSHA512(byte[] key, byte[] message) {
+        byte[] result = new byte[64];
+
+        HMac hmac = new HMac(new SHA512Digest());
+        hmac.init(new KeyParameter(key));
+        hmac.update(message, 0, message.length);
+        hmac.doFinal(result, 0);
+        return result;
+    }
+}
