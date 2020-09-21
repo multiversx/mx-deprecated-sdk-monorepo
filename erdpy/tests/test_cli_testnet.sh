223d9b18b311b37010171d6656c3bc452dd58c00
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-09-21 17:27:54 +0300

    Add new test (restart).

--- erdpy/tests/test_cli_testnet.sh
+++ erdpy/tests/test_cli_testnet.sh
@@ -2,7 +2,7 @@
 
 source "./shared.sh"
 
-testAll() {
+testStart() {
     pushd .
 
     cleanSandbox
@@ -13,6 +13,23 @@ testAll() {
     cp ./testdata/testnets/testnet_foo.toml ${SANDBOX}/testnet_foo/testnet.toml
     cd ${SANDBOX}/testnet_foo
     ${ERDPY} --verbose testnet config
+    ${ERDPY} --verbose testnet start
+
+    popd
+}
+
+testRestart() {
+    pushd .
+
+    cleanSandbox
+    mkdir -p ${SANDBOX}/testnet_foo
+
+    ${ERDPY} testnet prerequisites
+    
+    cp ./testdata/testnets/testnet_foo.toml ${SANDBOX}/testnet_foo/testnet.toml
+    cd ${SANDBOX}/testnet_foo
+    ${ERDPY} --verbose testnet config
+    ${ERDPY} --verbose testnet start
     ${ERDPY} --verbose testnet start
 
     popd
