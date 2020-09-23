55522c24362f8ae23c66643ac5fca57eaaeea703
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-09-23 11:58:07 +0300

    Use older toml version (bug in new version: https://github.com/uiri/toml/issues/217)

--- erdpy/testnet/node_config_toml.py
+++ erdpy/testnet/node_config_toml.py
@@ -3,8 +3,9 @@ from typing import Any
 from erdpy.testnet.config import TestnetConfiguration
 
 
-def patch(data: Any, testnet_config: TestnetConfiguration) -> Any:
+def patch(data: Any, testnet_config: TestnetConfiguration):
     data['GeneralSettings']['SCDeployEnableEpoch'] = 0
     data['GeneralSettings']['BuiltInFunctionsEnableEpoch'] = 0
     data['GeneralSettings']['RelayedTransactionsEnableEpoch'] = 0
     data['GeneralSettings']['PenalizedTooMuchGasEnableEpoch'] = 0
+    data['DbLookupExtensions']['Enabled'] = True
