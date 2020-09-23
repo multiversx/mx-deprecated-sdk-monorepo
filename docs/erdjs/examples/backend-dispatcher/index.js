2c867f349d664584db73be044a5096667266fc62
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-09-23 18:30:37 +0300

    Add "send-more-txs".

--- docs/erdjs/examples/backend-dispatcher/index.js
+++ docs/erdjs/examples/backend-dispatcher/index.js
@@ -34,6 +34,16 @@ function setupCli(program) {
         .action(sendTransaction);
 
     program
+        .command("send-more-txs")
+        .description("Send more transactions")
+        .requiredOption("-k, --key-file <keyFile>", "the key-file (the wallet)")
+        .requiredOption("-p, --password-file <passwordFile>", "the file containing the key-file password")
+        .requiredOption("-r, --receiver <receiver>", "the receiver (bech32 address)")
+        .requiredOption("-v, --value <value>", "the value, in eGLD")
+        .requiredOption("-n, --num <num>", "number of transactions to send")
+        .action(sendMoreTransactions);
+
+    program
         .command("get-tx")
         .description("Get transaction")
         .requiredOption("-h, --hash <hash>", "the hash of the transaction")
@@ -53,10 +63,24 @@ async function sendTransaction(cmdObj) {
     let signer = createSigner(cmdObj.keyFile, cmdObj.passwordFile);
     let sender = new Account(signer.getAddress());
     await sender.sync(provider);
+    await sendOneTransaction(provider, signer, sender, cmdObj.receiver, cmdObj.value);
+}
 
+function createSigner(keyFile, passwordFile) {
+    keyFile = asUserPath(keyFile);
+    passwordFile = asUserPath(passwordFile);
+
+    let keyFileJson = readText(keyFile);
+    let keyFileObject = JSON.parse(keyFileJson);
+    let password = readText(passwordFile);
+    let signer = SimpleSigner.fromWalletKey(keyFileObject, password);
+    return signer;
+}
+
+async function sendOneTransaction(provider, signer, sender, receiverAddress, valueInEGLD) {
     let nonce = sender.nonce;
-    let receiver = new Address(cmdObj.receiver);
-    let value = Balance.eGLD(cmdObj.value);
+    let receiver = new Address(receiverAddress);
+    let value = Balance.eGLD(valueInEGLD);
     let payload = new TransactionPayload("");
     let gasLimit = GasLimit.forTransfer(payload);
     
@@ -73,15 +97,20 @@ async function sendTransaction(cmdObj) {
     console.log(transaction);
 }
 
-function createSigner(keyFile, passwordFile) {
-    keyFile = asUserPath(keyFile);
-    passwordFile = asUserPath(passwordFile);
+async function sendMoreTransactions(cmdObj) {
+    let provider = new ProxyProvider(PROXY_URL);
+    NetworkConfig.getDefault().sync(provider);
 
-    let keyFileJson = readText(keyFile);
-    let keyFileObject = JSON.parse(keyFileJson);
-    let password = readText(passwordFile);
-    let signer = SimpleSigner.fromWalletKey(keyFileObject, password);
-    return signer;
+    let signer = createSigner(cmdObj.keyFile, cmdObj.passwordFile);
+    let sender = new Account(signer.getAddress());
+    await sender.sync(provider);
+
+    // When sending more transactions in a row, the sender has to manage its local copy of the nonce:
+    // https://docs.elrond.com/creating-transactions#nonce-management
+    for (let i = 0; i < cmdObj.num; i++) {
+        await sendOneTransaction(provider, signer, sender, cmdObj.receiver, cmdObj.value);
+        sender.incrementNonce();
+    }
 }
 
 async function getTransaction(cmdObj) {
