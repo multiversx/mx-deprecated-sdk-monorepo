8e791350068bd8b6b576652888e27a4b8b30bed0
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-10-01 13:56:44 +0300

    Add more tests.

--- erdjs/src/smartcontracts/transactionPayloadBuilders.spec.ts
+++ erdjs/src/smartcontracts/transactionPayloadBuilders.spec.ts
@@ -4,13 +4,35 @@ import { Address } from "../address";
 import { TransactionPayload } from "../transactionPayload";
 import { Argument } from "./argument";
 import { ContractFunction } from "./function";
+import { Code } from "./code";
+import { CodeMetadata } from "./codeMetadata";
 
 
 describe("test contract payload builders", () => {
-    let aliceBech32 = "erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz";
+    it("should prepare deploy correctly", async () => {
+        let payload = TransactionPayload
+            .contractDeploy()
+            .setCode(Code.fromBuffer(Buffer.from([1, 2, 3, 4])))
+            .setCodeMetadata(new CodeMetadata(true, false, true))
+            .addInitArgument(Argument.number(1024))
+            .build();
+
+        assert.equal(payload.decoded(), "01020304@0500@0102@0400");
+    });
+
+    it("should prepare upgrade correctly", async () => {
+        let payload = TransactionPayload
+            .contractUpgrade()
+            .setCode(Code.fromBuffer(Buffer.from([1, 2, 3, 4])))
+            .setCodeMetadata(new CodeMetadata(true, false, true))
+            .addInitArgument(Argument.number(1024))
+            .build();
+
+        assert.equal(payload.decoded(), "upgradeContract@01020304@0102@0400");
+    });
 
     it("should prepare call correctly", async () => {
-        let alice = new Address(aliceBech32);
+        let alice = new Address("erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz");
         let payload = TransactionPayload
             .contractCall()
             .setFunction(new ContractFunction("transferToken"))
