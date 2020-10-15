17daacfa8acc9c8caafd3a31ff1365eef6725b7d
Mihai Bârcă <mihai.barca@elrond.com>
2020-10-15 13:30:22 +0300

    fix after review

--- erdgo/address.go
+++ erdgo/address.go
@@ -11,6 +11,13 @@ const (
 	bech32Hrp        = "erd"
 	bech32AddressLen = 62
 	pubkeyLen        = 32
+
+	bech32EncodeFromBits = 8
+	bech32EncodeToBits   = 5
+	bech32EncodePadding  = true
+	bech32DecodeFromBits = 5
+	bech32DecodeToBits   = 8
+	bech32DecodePadding  = false
 )
 
 // IsValidBech32Address returns true if the provided parameter is a valid Elrond bech32 address and false otherwise
@@ -19,7 +26,7 @@ func IsValidBech32Address(address string) bool {
 	if err != nil || hrp != bech32Hrp {
 		return false
 	}
-	pubkey, err := bech32.ConvertBits(bytes, 5, 8, false)
+	pubkey, err := bech32.ConvertBits(bytes, bech32DecodeFromBits, bech32DecodeToBits, bech32DecodePadding)
 	if err != nil {
 		return false
 	}
@@ -42,7 +49,7 @@ func PubkeyToBech32(pubkey []byte) (string, error) {
 	if !IsValidPubkey(hex.EncodeToString(pubkey)) {
 		return "", errInvalidPubkey
 	}
-	bytes, err := bech32.ConvertBits(pubkey, 8, 5, true)
+	bytes, err := bech32.ConvertBits(pubkey, bech32EncodeFromBits, bech32EncodeToBits, bech32EncodePadding)
 	if err != nil {
 		return "", err
 	}
@@ -60,19 +67,28 @@ func Bech32ToPubkey(address string) ([]byte, error) {
 		return nil, errInvalidAddress
 	}
 	_, bytes, _ := bech32.Decode(address)
-	pubkey, _ := bech32.ConvertBits(bytes, 5, 8, false)
+	pubkey, _ := bech32.ConvertBits(bytes, bech32DecodeFromBits, bech32DecodeToBits, bech32DecodePadding)
 
 	return pubkey, nil
 }
 
 // GetAddressShard computes the shard of an address
 func GetAddressShard(address string, numShardsWithoutMeta uint32) (uint32, error) {
-	if !IsValidBech32Address(address) {
+	pubkey, err := Bech32ToPubkey(address)
+	if err != nil {
 		return 0, errInvalidAddress
+	}
+
+	return GetPubkeyShard(pubkey, numShardsWithoutMeta)
+}
+
+// GetPubkeyShard computes the shard of a public key
+func GetPubkeyShard(pubkey []byte, numShardsWithoutMeta uint32) (uint32, error) {
+	if !IsValidPubkey(hex.EncodeToString(pubkey)) {
+		return 0, errInvalidPubkey
 	}
 	n := math.Ceil(math.Log2(float64(numShardsWithoutMeta)))
 	var maskHigh, maskLow uint32 = (1 << uint(n)) - 1, (1 << uint(n-1)) - 1
-	pubkey, _ := Bech32ToPubkey(address)
 	addr := uint32(pubkey[len(pubkey)-1])
 	shard := addr & maskHigh
 	if shard > numShardsWithoutMeta-1 {
