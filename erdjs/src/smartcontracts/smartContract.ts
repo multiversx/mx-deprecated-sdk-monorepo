2b75b1c6bd6217adbcfaa8af0f2ad91dbe73e5d1
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-10-27 16:57:09 +0200

    Add comments, documentation.

--- erdjs/src/smartcontracts/smartContract.ts
+++ erdjs/src/smartcontracts/smartContract.ts
@@ -13,6 +13,9 @@ import { ArwenVirtualMachine } from "./transactionPayloadBuilders";
 import { Nonce } from "../nonce";
 import { ContractFunction } from "./function";
 
+/**
+ * An abstraction for deploying and interacting with Smart Contracts.
+ */
 export class SmartContract implements ISmartContract {
     private owner: Address = new Address();
     private address: Address = new Address();
@@ -21,42 +24,72 @@ export class SmartContract implements ISmartContract {
     private codeMetadata: CodeMetadata = new CodeMetadata();
     private readonly trackOfTransactions: Transaction[] = [];
 
+    /**
+     * Create a SmartContract object by providing its address on the Network.
+     */
     constructor({ address }: { address?: Address }) {
         if (address) {
             this.address = address;
         }
     }
 
+    /**
+     * Sets the address, as on Network.
+     */
     setAddress(address: Address) {
         this.address = address;
     }
 
+    /**
+     * Gets the address, as on Network.
+     */
     getAddress(): Address {
         this.address.assertNotEmpty();
         return this.address;
     }
 
+    /**
+     * Gets the owner address. 
+     * 
+     * Note that this function doesn't query the Network, but uses the information acquired when signing a deployment transaction.
+     * Therefore, currently, this function is useful only in the context of deploying Smart Contracts.
+     */
     getOwner(): Address {
         this.owner.assertNotEmpty();
         return this.owner;
     }
 
+    /**
+     * ABIs aren't currently supported by `erdjs`. They will be supported in a future version.
+     */
     setAbi(abi: Abi) {
         this.abi = abi;
     }
 
+    /**
+     * ABIs aren't currently supported by `erdjs`. They will be supported in a future version.
+     */
     getAbi(): Abi {
         return this.abi;
     }
 
+    /**
+     * Gets the {@link Code} of the Smart Contract. Does not query the Network.
+     */
     getCode(): Code {
         return this.code;
     }
 
+    /**
+     * Gets the {@link CodeMetadata} of the Smart Contract. Does not query the Network.
+     */
     getCodeMetadata(): CodeMetadata {
         return this.codeMetadata;
     }
 
+    /**
+     * Creates a {@link Transaction} for deploying the Smart Contract to the Network.
+     */
     deploy({ code, codeMetadata, initArguments, value, gasLimit }
         : { code: Code, codeMetadata?: CodeMetadata, initArguments?: Argument[], value?: Balance, gasLimit: GasLimit }
     ): Transaction {
@@ -93,6 +126,9 @@ export class SmartContract implements ISmartContract {
         this.trackOfTransactions.push(transaction);
     }
 
+    /**
+     * Creates a {@link Transaction} for upgrading the Smart Contract on the Network.
+     */
     upgrade({ code, codeMetadata, initArgs, value, gasLimit }
         : { code: Code, codeMetadata?: CodeMetadata, initArgs?: Argument[], value?: Balance, gasLimit: GasLimit }): Transaction {
         codeMetadata = codeMetadata || new CodeMetadata();
@@ -123,6 +159,9 @@ export class SmartContract implements ISmartContract {
         this.trackOfTransactions.push(transaction);
     }
 
+    /**
+     * Creates a {@link Transaction} for calling (a function of) the Smart Contract.
+     */ 
     call({ func, args, value, gasLimit }
         : { func: ContractFunction, args?: Argument[], value?: Balance, gasLimit: GasLimit }): Transaction {
         args = args || [];
@@ -149,6 +188,13 @@ export class SmartContract implements ISmartContract {
         this.trackOfTransactions.push(transaction);
     }
 
+    /**
+     * Computes the address of a Smart Contract. 
+     * The address is computed deterministically, from the address of the owner and the nonce of the deployment transaction.
+     * 
+     * @param owner The owner of the Smart Contract
+     * @param nonce The owner nonce used for the deployment transaction
+     */
     static computeAddress(owner: Address, nonce: Nonce): Address {
         let initialPadding = Buffer.alloc(8, 0);
         let ownerPubkey = owner.pubkey();
