014f5815aa885d4d2f0bf3916232bb2d4e8a04a0
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-10-28 12:55:25 +0200

    Add dependency tag for elrond-wasm-rs and update get_templates_repository()

--- erdpy/projects/templates_config.py
+++ erdpy/projects/templates_config.py
@@ -1,10 +1,13 @@
 import time
 
+from erdpy import config
 from erdpy.projects.templates_repository import TemplatesRepository
 
 
 def get_templates_repositories():
     timestamp = int(time.time())
+    examples_rs_tag = config.get_dependency_tag('elrond_wasm_rs')
+    examples_rs_tag_no_v = examples_rs_tag[1:]
 
     return [
         TemplatesRepository(
@@ -16,8 +19,8 @@ def get_templates_repositories():
 
         TemplatesRepository(
             key="elrond-wasm-rs",
-            url=f"https://github.com/ElrondNetwork/elrond-wasm-rs/archive/master.zip?t={timestamp}",
+            url=f"https://github.com/ElrondNetwork/elrond-wasm-rs/archive/{examples_rs_tag}.zip?t={timestamp}",
             github="ElrondNetwork/elrond-wasm-rs",
-            relative_path="elrond-wasm-rs-master/examples"
+            relative_path=f"elrond-wasm-rs-{examples_rs_tag_no_v}/examples"
         )
     ]
