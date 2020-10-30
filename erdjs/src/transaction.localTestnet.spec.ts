e821d0c3caaa4da23902a5516cb1c1feebd69149
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-10-30 16:59:20 +0200

    Add extra test (sending transactions).

--- erdjs/src/transaction.localTestnet.spec.ts
+++ erdjs/src/transaction.localTestnet.spec.ts
@@ -7,12 +7,55 @@ import { Balance } from "./balance";
 import { TestWallets } from "./testutils/wallets";
 import { describeOnlyIf, getLocalTestnetProvider } from "./testutils/utils";
 import { Logger } from "./logger";
+import { assert } from "chai";
 
-describeOnlyIf("localTestnet")("test transaction", () => {
+describeOnlyIf("localTestnet")("test transaction", function () {
+    this.timeout(20000);
+
     let wallets = new TestWallets();
     let aliceWallet = wallets.alice;
     let alice = new Account(aliceWallet.address);
     let aliceSigner = aliceWallet.signer;
+    let bobWallet = wallets.bob;
+    let bob = new Account(bobWallet.address);
+
+    it("should send transactions", async () => {
+        let localTestnet = getLocalTestnetProvider();
+
+        await NetworkConfig.getDefault().sync(localTestnet);
+        await alice.sync(localTestnet);
+
+        await bob.sync(localTestnet);
+        let initialBalanceOfBob = bob.balance;
+
+        let transactionOne = new Transaction({
+            receiver: bob.address,
+            value: Balance.eGLD(42)
+        });
+
+        let transactionTwo = new Transaction({
+            receiver: bob.address,
+            value: Balance.eGLD(43)
+        });
+
+        transactionOne.setNonce(alice.nonce);
+        alice.incrementNonce();
+        transactionTwo.setNonce(alice.nonce);
+
+        await aliceSigner.sign(transactionOne);
+        await aliceSigner.sign(transactionTwo);
+
+        await transactionOne.send(localTestnet);
+        await transactionTwo.send(localTestnet);
+
+        await transactionOne.awaitExecuted(localTestnet);
+        await transactionTwo.awaitExecuted(localTestnet);
+
+        await bob.sync(localTestnet);
+        let newBalanceOfBob = bob.balance;
+
+        assert.equal(Balance.eGLD(85).value, newBalanceOfBob.value - initialBalanceOfBob.value);
+    });
 
     it("should simulate transactions", async () => {
         let localTestnet = getLocalTestnetProvider();
