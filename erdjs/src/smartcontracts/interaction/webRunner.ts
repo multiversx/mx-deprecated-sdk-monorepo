f2f15c25d185cf6ab1254f2ad2dc141890d458d9
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-12-14 22:34:18 +0200

    WebRunner (not implemented).

--- erdjs/src/smartcontracts/interaction/webRunner.ts
+++ erdjs/src/smartcontracts/interaction/webRunner.ts
@@ -1,21 +1,27 @@
 import { Address } from "../../address";
-import { TransactionHash } from "../../transaction";
-import { QueryResponse } from "../query";
+import { Transaction, TransactionHash, TransactionOnNetwork } from "../../transaction";
+import { Query, QueryResponse } from "../query";
+import { Interaction } from "./interaction";
 import { IInteractionRunner } from "./interface";
-import { PreparedInteraction } from "./preparedInteraction";
 
 export class WebInteractionRunner implements IInteractionRunner {
-    // TODO: Use WalletProvider. No ProxyProvider.
-
-    runBroadcast(_interaction: PreparedInteraction): Promise<TransactionHash> {
+    checkInteraction(_interaction: Interaction): void {
         throw new Error("Method not implemented.");
     }
 
-    runQuery(_interaction: PreparedInteraction, _caller?: Address): Promise<QueryResponse> {
+    broadcast(_transaction: Transaction): Promise<TransactionHash> {
         throw new Error("Method not implemented.");
     }
 
-    runSimulate(_interaction: PreparedInteraction): Promise<any> {
+    broadcastAwaitExecution(_transaction: Transaction): Promise<TransactionOnNetwork> {
+        throw new Error("Method not implemented.");
+    }
+
+    query(_query: Query, _caller?: Address): Promise<QueryResponse> {
+        throw new Error("Method not implemented.");
+    }
+
+    simulate(_transaction: Transaction): Promise<any> {
         throw new Error("Method not implemented.");
     }
 }
