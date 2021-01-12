975bbc644f92adf0609a0ad9756b215b0c7898e6
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2021-01-12 18:20:37 +0200

    No more namespaces.

--- erdjs/src/smartcontracts/typesystem/abiRegistry.spec.ts
+++ erdjs/src/smartcontracts/typesystem/abiRegistry.spec.ts
@@ -2,7 +2,7 @@ import { assert } from "chai";
 import { AbiRegistry } from "./abiRegistry";
 
 describe("test abi registry", () => {
-    it("should extend with namespaces and without namespaces", async () => {
+    it("should extend", async () => {
         let registry = new AbiRegistry();
 
         await registry.extendFromFile("src/testdata/answer.json");
@@ -19,13 +19,5 @@ describe("test abi registry", () => {
         assert.lengthOf(registry.interfaces, 3);
         assert.lengthOf(registry.structures, 1);
         assert.lengthOf(registry.findInterface("lottery-egld").endpoints, 3);
-
-        await registry.extendFromFile("src/testdata/foobar.namespaced.json");
-        assert.lengthOf(registry.interfaces, 3);
-        assert.lengthOf(registry.structures, 1);
-        assert.lengthOf(registry.interfaces, 3);
-        assert.lengthOf(registry.structures, 1);
-        assert.lengthOf(registry.interfaces, 1);
-        assert.lengthOf(registry.structures, 1);
     });
 });
