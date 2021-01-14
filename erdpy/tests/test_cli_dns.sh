b3ae278db24dda2b649c014d4bc5dd9ab066f117
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2021-01-14 20:06:03 +0200

    Extra tests for dns CLI.

--- erdpy/tests/test_cli_dns.sh
+++ erdpy/tests/test_cli_dns.sh
@@ -17,15 +17,95 @@ testOnline() {
 testRegistrationOnline() {
     ${ERDPY} --verbose dns register --name="alicesmith" --pem=${ALICE} --value=${REGISTRATION_COST} \
         --recall-nonce --gas-limit=100000000 --gas-price=1000000000 --chain=${CHAIN_ID} \
-        --outfile=${SANDBOX}/txRegister.txt --send --proxy=${PROXY} || return 1
+        --outfile=${SANDBOX}/txRegisterAlice.txt --send --proxy=${PROXY} || return 1
 
     ${ERDPY} --verbose dns register --name="bobbysmith" --pem=${BOB} --value=${REGISTRATION_COST} \
         --recall-nonce --gas-limit=100000000 --gas-price=1000000000 --chain=${CHAIN_ID} \
-        --outfile=${SANDBOX}/txRegister.txt --send --proxy=${PROXY} || return 1
+        --outfile=${SANDBOX}/txRegisterBob.txt --send --proxy=${PROXY} || return 1
 }
 
 testTransactionsWithUsernamesOnline() {
-    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} --receiver-username="bobbysmith" \
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} \
         --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
         --outfile=${SANDBOX}/txA.txt --send --proxy=${PROXY} || return 1
+
+    sleep 10
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} --receiver-username="bobbysmith" \
+        --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txB.txt --send --proxy=${PROXY} || return 1
+
+    sleep 10
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} --receiver-username="bobbysmithfoo" \
+        --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txC.txt --send --proxy=${PROXY} || return 1
+
+    sleep 10
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmith" --receiver=${BOB_ADDRESS} --receiver-username="bobbysmith" \
+        --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txD.txt --send --proxy=${PROXY} || return 1
+
+    sleep 10
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmith" --receiver=${BOB_ADDRESS} \
+        --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txF.txt --send --proxy=${PROXY} || return 1
+
+    sleep 10
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmithfoo" --receiver=${BOB_ADDRESS} \
+        --value="1${DENOMINATION}" --recall-nonce --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txG.txt --send --proxy=${PROXY} || return 1
+}
+
+
+testOffline() {
+    testRegistrationOffline || return 1
+    testTransactionsWithUsernamesOffline || return 1
+}
+
+testRegistrationOffline() {
+    ${ERDPY} --verbose dns register --name="alicesmith" --pem=${ALICE} --value=${REGISTRATION_COST} \
+        --nonce=7 --gas-limit=100000000 --gas-price=1000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txRegisterAlice.txt || return 1
+    assertFileExists ${SANDBOX}/txRegisterAlice.txt || return 1
+
+    ${ERDPY} --verbose dns register --name="bobbysmith" --pem=${BOB} --value=${REGISTRATION_COST} \
+        --nonce=8 --gas-limit=100000000 --gas-price=1000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txRegisterBob.txt || return 1
+    assertFileExists ${SANDBOX}/txRegisterBob.txt || return 1
+}
+
+testTransactionsWithUsernamesOffline() {
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} \
+        --value="1${DENOMINATION}" --nonce=42 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txA.txt || return 1
+    assertFileExists ${SANDBOX}/txA.txt || return 1
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} --receiver-username="bobbysmith" \
+        --value="1${DENOMINATION}" --nonce=43 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txB.txt || return 1
+    assertFileExists ${SANDBOX}/txB.txt || return 1
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --receiver=${BOB_ADDRESS} --receiver-username="bobbysmithfoo" \
+        --value="1${DENOMINATION}" --nonce=44 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txC.txt || return 1
+    assertFileExists ${SANDBOX}/txC.txt || return 1
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmith" --receiver=${BOB_ADDRESS} --receiver-username="bobbysmith" \
+        --value="1${DENOMINATION}" --nonce=45 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txD.txt || return 1
+    assertFileExists ${SANDBOX}/txD.txt || return 1
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmith" --receiver=${BOB_ADDRESS} \
+        --value="1${DENOMINATION}" --nonce=46 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txF.txt || return 1
+    assertFileExists ${SANDBOX}/txF.txt || return 1
+
+    ${ERDPY} --verbose tx new --pem=${ALICE} --sender-username="alicesmithfoo" --receiver=${BOB_ADDRESS} \
+        --value="1${DENOMINATION}" --nonce=47 --gas-limit=50000 --gas-price=2000000000 --chain=${CHAIN_ID} \
+        --outfile=${SANDBOX}/txG.txt || return 1
+    assertFileExists ${SANDBOX}/txG.txt || return 1
 }
