50dd2cfcb31f7900bdb1f06180940b32d94a2d2c
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2021-04-04 22:41:40 +0300

    Fix exports.

--- erdjs/src/smartcontracts/index.ts
+++ erdjs/src/smartcontracts/index.ts
@@ -3,11 +3,18 @@
  * @module smartcontracts
  */
 
+export * from "./abi";
+export * from "./argSerializer";
 export * from "./code";
 export * from "./codeMetadata";
+export * from "./defaultRunner";
 export * from "./function";
+export * from "./interaction";
 export * from "./interface";
 export * from "./query";
+export * from "./queryResponse";
 export * from "./returnCode";
 export * from "./smartContract";
+export * from "./smartContractResults";
+export * from "./strictChecker";
 export * from "./transactionPayloadBuilders";
